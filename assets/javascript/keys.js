1d03fadbf13ba3f4e5b58c27c14c98c055a0b764
Dana Cronin <cronin43224235@gmail.com>
2019-11-13 17:25:40 -0500

    spotify dev account

--- assets/javascript/keys.js
+++ assets/javascript/keys.js
@@ -1,6 +1,21 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.f4d9611db9a04944949d90f62daec527,
+  secret: process.env.b0a6bb6df122452598618386b424ad48
 };
+
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
+
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
+
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
