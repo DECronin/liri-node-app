128715ddb987e470c37a954c3928dd6186d9a19c
Dana Cronin <cronin43224235@gmail.com>
2019-11-17 14:13:35 -0500

    fixing key syntax

--- keys.js
+++ keys.js
@@ -1,22 +1,22 @@
 console.log('this is loaded');
 
-exports.spotify = {
-  id: process.env.f4d9611db9a04944949d90f62daec527,
-  secret: process.env.b0a6bb6df122452598618386b424ad48
-};
-
-exports.imdb = {
-  key: '96fd9cbd'
-};
-
-exports.bands = {
-  provider: "trilogy",
-  apiKey: "codingbootcamp"
-}
-
-
-
-// exports.spotify = {
-//   id: process.env.SPOTIFY_ID,
-//   secret: process.env.SPOTIFY_SECRET
+// exports.imdb = {
+//   key: '96fd9cbd'
 // };
+
+// exports.bands = {
+//   provider: "trilogy",
+//   apiKey: "codingbootcamp"
+// }
+
+
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
