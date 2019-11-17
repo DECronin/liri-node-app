ee70a2a728e9c49ffc2c23b4e4c72627460dc9b6
Dana Cronin <cronin43224235@gmail.com>
2019-11-17 15:26:23 -0500

    default value in concet-query and successfull spotify connection

--- liri.js
+++ liri.js
@@ -2,38 +2,39 @@ require("dotenv").config();
 const moment = require('moment');
 const axios = require("axios");
 const keys = require("./keys.js");
-let webAddress;
-let queryParams;
-// let queryUrl = `http://${webAddress}.com/${queryParams}`;
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
-// var spotify = new Spotify(keys.spotify);
-// let queryURL = `https://rest.bandsintown.com/artists/` + artist + "?app_id=codingbootcamp";
-
-var spooph = `https://accounts.spotify.com/v1/?search='icon+for+hire'`;
+// var spooph = `https://accounts.spotify.com/v1/?search='icon+for+hire'`;
 
 switch (process.argv[2]) {
     case 'concert-this':
-        let url = `https://rest.bandsintown.com/artists/${process.argv.slice(3).join('+')}/events?app_id=codingbootcamp`;
+        let url = `https://rest.bandsintown.com/artists/${process.argv.slice(3).join('+') || 'fall+out+boy'}/events?app_id=codingbootcamp`;
         axios.get(url).then(function (res) {
             console.log(`Venue Name: ${res.data[0].venue.name}\nLocation: ${res.data[0].venue.city}, ${res.data[0].venue.country}\nEvent Date: ${moment(res.data[0].datetime).format('MMMM Do YYYY, h:mm a')}`);
         });
         break;
     case 'spotify-this-song': console.log('spotify ================================================');
-        axios.get(spooph).then(function (response) {
-            console.log(response.data);
-            console.log("connected");
+        spotify.search({ type: 'track', query: process.argv.slice(3).join(' ') || 'All the Small Things'}, function(err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            console.log(data); 
         });
-        // var newQueryUrl = 
-        // webAddress = 'api.spotify'; 
-        // queryParams = 'v1/search?q=track';
         break;
-    case 'movie-this': webAddress = '';
-        webAddress = '';
-        queryParams = '';
+    case 'movie-this': 
+
         break;
-    case 'do-what-it-says': webAddress = '';
-        webAddress = '';
-        queryParams = '';
+    case 'do-what-it-says': 
+        
         break;
-    default: console.log('Please give an apropriate command.');
+    default: console.log('Please give an apropriate command.'); break;
 }
+
+// spotify
+// =======
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+// default to "The Sign" by Ace of Base.
