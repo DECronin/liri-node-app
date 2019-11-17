d473f250a43d14e2227a52383c0037150251170e
Dana Cronin <cronin43224235@gmail.com>
2019-11-17 16:45:26 -0500

    imdb linked and console.log started

--- liri.js
+++ liri.js
@@ -4,17 +4,18 @@ const axios = require("axios");
 const keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
+let url = '';
 
 // var spooph = `https://accounts.spotify.com/v1/?search='icon+for+hire'`;
 
 switch (process.argv[2]) {
     case 'concert-this':
-        let url = `https://rest.bandsintown.com/artists/${process.argv.slice(3).join('+') || 'fall+out+boy'}/events?app_id=codingbootcamp`;
+        url = `https://rest.bandsintown.com/artists/${process.argv.slice(3).join('+') || 'fall+out+boy'}/events?app_id=codingbootcamp`;
         axios.get(url).then(function (res) {
             console.log(`Venue Name: ${res.data[0].venue.name}\nLocation: ${res.data[0].venue.city}, ${res.data[0].venue.country}\nEvent Date: ${moment(res.data[0].datetime).format('MMMM Do YYYY, h:mm a')}`);
         });
         break;
-    case 'spotify-this-song': console.log('spotify ================================================');
+    case 'spotify-this-song':
         spotify.search({ type: 'track', query: process.argv.slice(3).join(' ') || 'The Sign'}, function(err, data) {
             if (err) {
                 return console.log('Error occurred: ' + err);
@@ -23,7 +24,11 @@ switch (process.argv[2]) {
         });
         break;
     case 'movie-this': 
-
+        url = `https://www.omdbapi.com/?t=${process.argv.slice(3).join('+') || 'mr+nobody'}&apikey=trilogy`
+        axios.get(url).then(function (res) {
+            console.log(res.data);
+            // console.log(`Movie Title: ${res.data}\nRelease Year: ${res.data}\nIMDB Rating: ${res.data}\nRotten Tomatoes Rating: ${}\nProduction Country: ${}\nOriginal Language: ${}\nPlot: ${}\nActors: ${}`);
+        });
         break;
     case 'do-what-it-says': 
         // let dataBuffer = fs.readFileSync('random.txt');
@@ -34,11 +39,3 @@ switch (process.argv[2]) {
         break;
     default: console.log('Please give an apropriate command.'); break;
 }
-
-// spotify
-// =======
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
-// default to "The Sign" by Ace of Base.
