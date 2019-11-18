4c02d6dff1866633a94ffd6fcb370644373747cf
Dana Cronin <cronin43224235@gmail.com>
2019-11-17 22:44:38 -0500

    removing unessesary stuff

--- keys.js
+++ keys.js
@@ -1,21 +1,5 @@
 console.log('this is loaded');
 
-// exports.imdb = {
-//   key: '96fd9cbd'
-// };
-
-// exports.bands = {
-//   provider: "trilogy",
-//   apiKey: "codingbootcamp"
-// }
-
-
-
-// exports.spotify = {
-//   id: process.env.SPOTIFY_ID,
-//   secret: process.env.SPOTIFY_SECRET
-// };
-
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
